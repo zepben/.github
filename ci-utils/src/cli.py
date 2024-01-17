ffa952fa7d76ced1fbf6d21e7249cc2e2882b12a
Alex Lourie <alex.lourie@zepben.com>
2024-01-17 14:17:30 +1030

    initial new ci, create_branch implemented
    Signed-off-by: Alex Lourie <alex.lourie@zepben.com>

---
+++ ci-utils/src/cli.py
@@ -0,0 +1,66 @@
+import os
+import sys
+
+import click
+
+
+class Environment:
+    def __init__(self):
+        self.verbose = False
+        self.home = os.getcwd()
+
+    def log(self, msg, *args):
+        """Logs a message to stderr."""
+        if args:
+            msg %= args
+        click.echo(msg, file=sys.stderr)
+
+    def debug(self, msg, *args):
+        """Logs a message to stderr only if verbose is enabled."""
+        if self.verbose:
+            self.log(msg, *args)
+
+    def err(self, msg, *args):
+        """ Logs an error to stdout """
+        click.echo(msg, *args)
+
+    def warn(self, msg, *args):
+        """ Logs an warning to stdout """
+        print(" ctx.warn is not implemented yet!")
+        pass
+
+
+pass_environment = click.make_pass_decorator(Environment, ensure=True)
+cmd_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "commands"))
+
+
+class ComplexCLI(click.Group):
+    def list_commands(self, ctx):
+        rv = []
+        for filename in os.listdir(cmd_folder):
+            if filename.endswith(".py") and filename.startswith("cmd_"):
+                rv.append(filename[4:-3])
+        rv.sort()
+        return rv
+
+    def get_command(self, ctx, name):
+        try:
+            mod = __import__(f"src.commands.cmd_{name}", None, None, ["cli"])
+        except ImportError:
+            return
+        return mod.cli
+
+
+@click.command(cls=ComplexCLI)
+@click.option(
+    "--home",
+    type=click.Path(exists=True, file_okay=False, resolve_path=True),
+    help="Changes the folder to operate on.",
+)
+@click.option("-v", "--verbose", is_flag=True, help="Enables verbose mode.")
+@pass_environment
+def cli(ctx, verbose, home):
+    """A complex command line interface."""
+    ctx.verbose = verbose
+    if home is not None:
+        ctx.home = home
