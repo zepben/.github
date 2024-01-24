39bffcf04dd73493fa9a188061af48eaa34608f4
Alex Lourie <alex.lourie@zepben.com>
2024-01-25 02:20:18 +1030

    Adding more git wrappers
    Signed-off-by: Alex Lourie <alex.lourie@zepben.com>

--- ci-utils/src/utils/git.py
+++ ci-utils/src/utils/git.py
@@ -8,20 +8,20 @@ class Git():
 
     def __init__(self, ctx):
         self.ctx = ctx
-        self.remote_refs = {"heads": None, "tags": None}
+        self.remote_refs = {"heads": [], "tags": []}
         self.init_git()
 
-    def init_git(self) -> Repo:
+    def init_git(self):
         self.repo = Repo(os.getcwd())
 
     # TODO: consider renaming to fetch_remotes() and just use the object field
     # Also, obviously, test if we even need to use ls_remotes or local tags are ok
-    def ls_remotes(self) -> iter:
+    def ls_remotes(self):
         # git ls-remote is not wrapped yet
         if self.remote_refs["heads"]:
             return self.remote_refs
 
-        def parse_refs(ref: str) -> dict[str: list[str]]:
+        def parse_refs(ref: str):
             found_ref = re.search(".*(?P<ref_type>heads|tags).*", ref)
             if found_ref:
                 ref_type = found_ref.group("ref_type")
@@ -34,9 +34,17 @@ class Git():
         return self.remote_refs
 
     def delete_remote_branch(self, branch: str):
-        self.ctx.fail("GIT DELETE BRANCH not implemented yet")
+        self.repo.remotes.origin.push(refspec=(f":{branch}"))
 
     def checkout(self, branch: str):
+        ref = ""
+        current = "main"
+        if branch in self.repo.tags:
+            ref = self.repo.commit(branch)
+        else:
+            ref = self.repo.create_head(branch, current)
+        self.repo.head.reference = ref
+        self.repo.head.reset(index=True, working_tree=True)
         self.ctx.fail("GIT CHECKOUT BRANCH not implemented yet")
 
     def stage(self):
@@ -45,8 +53,8 @@ class Git():
     def status(self):
         self.ctx.fail("GIT STATUS not implemented yet")
 
-    def commit(self, comment: str = None):
-        if not comment:
+    def commit(self, comment: str = ""):
+        if len(comment) == 0:
             self.ctx.fail("Cannot commit code without comment!")
 
         self.ctx.fail("GIT COMMIT is not yet implemented!")
@@ -54,8 +62,8 @@ class Git():
     def push(self, remote, branch):
         self.ctx.fail("Push is not yet implemented!")
 
-    def commit_update_version(self, branch: str = None):
-        if not branch:
+    def commit_update_version(self, branch: str = ""):
+        if len(branch) == 0:
             branch = "release"
 
         self.ctx.info(f"Commiting changes to {branch}...")
